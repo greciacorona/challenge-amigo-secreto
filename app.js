241b961cd47317d108a6b2b9632b1f2da6347236
greciacorona <grecia.corona@gmail.com>
2025-08-26 20:46:21 -0600

    array para almacenar nombres y funcion agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-alert('Hola Mundo');
+
+let amigo
+let amigos = [];
+
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function agregarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+    if (amigo === "") {
+        alert("Por favor, inserte un nombre.");
+    } else {
+        amigos.push(amigo);
+        console.log(amigo);
+        asignarTextoElemento('ul', amigo);
+        limpiarCaja();
+    }
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
+    
+}
+
+//asignarTextoElemento('ul', 'Grecia');
